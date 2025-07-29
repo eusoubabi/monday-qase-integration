342afec4fc0983cb2f86c10018879620e85ca905
Bárbara Rodrigues Lopes <barbara.rodrigues@watch.tv.br>
2025-07-28 23:15:36 -0400

    corrigindo conexão

--- src/services/mondayQaseService.ts
+++ src/services/mondayQaseService.ts
@@ -1,15 +1,23 @@
 import pool from '../db';
 
 export const saveMapping = async (testPlanId: string, itemId: string) => {
-  await pool.query(
-    `INSERT INTO monday_qase (test_plan_id, item_id)
-     VALUES ($1, $2)
-     ON CONFLICT (test_plan_id) DO UPDATE SET item_id = EXCLUDED.item_id`,
-    [testPlanId, itemId]
-  );
+    await pool.query(
+        `INSERT INTO monday_qase_mapping (test_plan_id, item_id)
+         VALUES ($1, $2)
+         ON CONFLICT (test_plan_id) DO UPDATE SET item_id = EXCLUDED.item_id`,
+        [testPlanId, itemId]
+    );
 };
 
 export const getItemIdByTestPlan = async (testPlanId: string): Promise<string | null> => {
-  const result = await pool.query('SELECT item_id FROM monday_qase WHERE test_plan_id = $1', [testPlanId]);
-  return result.rows[0].item_id; // <- Aqui está o problema
+    const result = await pool.query(
+        'SELECT item_id FROM monday_qase_mapping WHERE test_plan_id = $1',
+        [testPlanId]
+    );
+
+    if (result.rows.length === 0) {
+        return null;
+    }
+
+    return result.rows[0].item_id;
 };
